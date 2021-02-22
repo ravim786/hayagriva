5eda5b674edb18274ada1c327bf43527b6504518
Ravi Mangalagiri <ravi.mangalagiri@outlook.com>
2021-02-21 21:28:51 -0500

    added DecisionTable

---
+++ src/main/java/com/bluepitch/ruleapp/model/LoanApplication.java
@@ -0,0 +1,8 @@
+package com.bluepitch.ruleapp.model;
+
+import lombok.Data;
+
+@Data
+public class LoanApplication {
+    private SubjectProperty subjectLoan;
+}
---
+++ src/main/java/com/bluepitch/ruleapp/model/SubjectProperty.java
@@ -0,0 +1,13 @@
+package com.bluepitch.ruleapp.model;
+
+import lombok.Data;
+
+@Data
+public class SubjectProperty {
+    private String attachmentType;
+    private String constructionMethodType;
+    private String constructionMethodTypeOtherDescription;
+    private String propertyProjectDesignType;
+    private String projectLegalStructureType;
+    private boolean pudIndicator;
+}
